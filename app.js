fbd5975bcb1411a125010bddeab8de6a0118f732
eklipsorz <eklipsorz@protonmail.ch>
2021-12-15 17:36:05 +0800

    feat: add test data and define view engine

--- app.js
+++ app.js
@@ -2,9 +2,33 @@
 const express = require('express')
 const { create } = require('express-handlebars')
 
-
-
-
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
 
 
 
@@ -14,9 +38,26 @@ const app = express()
 // define a port for express server
 const port = 3500
 
+const options = {
+  extname: '.hbs',
+  layoutsDir: 'views/layouts',
+  defaultLayout: 'main'
+}
+
+const handlebars = create(options)
+
+// set view engine
+app.engine('.hbs', handlebars.engine)
+app.set('view engine', '.hbs')
+
+// set views path
+app.set('views', process.cwd() + '/views')
+
+app.use('/', express.static('public'))
+
 
 app.get('/', (req, res) => {
-  console.log('hi')
+  res.render('index')
 })
 
 
