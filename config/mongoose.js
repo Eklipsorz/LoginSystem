5c8c0e62e5a49a6d21e6f05fe0d5f3b256eca63e
eklipsorz <eklipsorz@protonmail.ch>
2021-12-15 17:25:22 +0800

    feat: add monngoose.js into config

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+// define a port for db server and db name
+const port = 27017
+const dbName = 'loginAuth'
+
+mongoose.connect(`mongodb://localhost:${port}/${dbName}`)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoose connection failed')
+})
+
+db.once('open', () => {
+  console.log("mongoose connection successfully")
+})
+
+exports = module.exports = db
